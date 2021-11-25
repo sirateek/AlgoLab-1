3ced0cca83945084c2b4ff22f8e1f45cfe88cefc
Siratee K <sirateek@gmail.com>
2021-11-26 00:55:24 +0700

    Done uva10004_bicoloring

---
+++ adtlab/uva10004_bicoloring/main.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+vector<int> adj[200010];
+bool seen[200010];
+// int levels[100010];
+int color[200010];
+
+int n, m;
+
+bool bi_bfs(int s) {
+  list<int> q;
+  //   levels[s] = 0;
+  q.push_back(s);
+  seen[s] = true;
+
+  while (!q.empty()) {
+    int u = q.front();
+    q.pop_front();
+
+    // int l = levels[u];
+    for (int d = 0; d < adj[u].size(); d++) {
+      int v = adj[u][d];
+      if (!seen[v]) {
+        seen[v] = true;
+        // levels[v] = l + 1;
+        color[v] = 1 - color[u];
+        q.push_back(v);
+      }
+
+      if (color[v] == color[u]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+void input_graph() {
+  for (int i = 0; i < n; i++) {
+    adj[i].clear();
+  }
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  // Init seen
+  for (int i = 0; i < n; i++) {
+    seen[i] = false;
+  }
+
+  bool result = bi_bfs(0);
+  if (result) {
+    cout << "BICOLORABLE." << endl;
+
+  } else {
+    cout << "NOT BICOLORABLE." << endl;
+  }
+}
+
+int main() {
+  while (true) {
+    cin >> n;
+    if (n == 0) {
+      break;
+    }
+    cin >> m;
+    input_graph();
+  }
+  return 0;
+}
